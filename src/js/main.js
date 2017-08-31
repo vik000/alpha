7bd15f94e9e184d554785d6e96045e33ffe07a68
Victor Manuel García Sanabria <vik000@gmail.com>
2017-08-31 13:38:27 +0200

    Bootstrap 4 instalado con npm, boostrap css importados con @import desde node_modules, comprobado que se compilan en styles.css dentro de dist/ con gulp sass. Falta comprobar que los css se heredan y enlazan bien en el html (aunque no veo por qué no).

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,3 @@
 window.jQuery = $ = require('jquery');
-//var bootstrap = require('bootstrap/dist/js/bootstrap'); //Esto lo ponemos cuando instalemos bootstrap (que tiene que ser por npm). Con esto ya debería ir.
+var bootstrap = require('bootstrap/dist/js/bootstrap'); //Esto lo ponemos cuando instalemos bootstrap (que tiene que ser por npm). Con esto ya debería ir.
 console.log("hi");
