a26f65bb3bdecc9dd8536d503cfac5975a88fec9
Victor Manuel García Sanabria <vik000@gmail.com>
2017-08-31 09:50:13 +0200

    Scaffolding pre Browserify (gulp, gulp watch, gulp html, gulp sass y todo compilado en dist desde src, con componentes bien importados y sincronizado con browserSync)

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var htmlImport = require("gulp-html-import");
+var browserSync = require('browser-sync').create();
+
+gulp.task('default', function(){
+  browserSync.init({server:"dist/"});
+  gulp.watch(["src/scss/*.scss","src/scss/**/*.scss"],["sass"]);
+});
+
+gulp.task('sass', function(){
+  gulp.src("src/scss/style.scss")
+  .pipe(sass().on("error", sass.logError))
+  .pipe(gulp.dest("./dist/styles"))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('html', function () {
+    gulp.src('./src/index.html')
+        .pipe(htmlImport('src/components/')) //aquí debería ir gulpImport, pero con eso no sale y me dice que no está definido, mientras que com lo que jhay puesto sí que sale.
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.stream());
+  });
