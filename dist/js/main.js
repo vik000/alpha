8522f0858320c650e55288ed0925b93b034255cb
Victor Manuel García Sanabria <vik000@gmail.com>
2017-08-31 14:27:31 +0200

    Instalado jquery (putas comillas, ojo con poner las que son) y bootstrap. Aún no funciona porque da un error de dependencias: falta popper.js (de momento). Mientras lo investigo, un commit de lo que llevo.

--- dist/js/main.js
+++ dist/js/main.js
@@ -14088,7 +14088,8 @@ return jQuery;
 },{}],3:[function(require,module,exports){
 'use strict';
 
-window.jQuery = $ = require('jquery');
+window.jQuery = window.$ = require('jquery');
+console.log($);
 var bootstrap = require('bootstrap/dist/js/bootstrap'); //Esto lo ponemos cuando instalemos bootstrap (que tiene que ser por npm). Con esto ya debería ir.
 console.log("hi");
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
-window.jQuery = $ = require('jquery');
+window.jQuery = window.$ = require('jquery');
+console.log($);
 var bootstrap = require('bootstrap/dist/js/bootstrap'); //Esto lo ponemos cuando instalemos bootstrap (que tiene que ser por npm). Con esto ya debería ir.
 console.log("hi");
